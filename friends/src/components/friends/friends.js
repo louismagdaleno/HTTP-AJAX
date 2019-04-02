509eb18c763bd82a98d7ef5fa871b36ef2c9455f
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-01 21:42:56 -0700

    Added friend component

--- friends/src/components/friends/friends.js
+++ friends/src/components/friends/friends.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Friend from '../friend/friend';
 
 const Friends = props => {
     return (
         <div className="friends">
-           
+           {props.friends.map(friend => <Friend friend={friend}/>)}
         </div>
     );
 }
