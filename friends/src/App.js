b681e3be93bfadd0750e5bd0b2efcd0412612391
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-01 21:38:34 -0700

    Added axios call to friends api

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data : []
+  }
+  componentDidMount(){
+    axios.get('http://localhost:5000/friends')
+      .then(response => this.setState({
+        data: response.data
+      }))
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
---
+++ friends/src/components/friends/friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = props => {
+    return (
+        <div className="friends">
+           
+        </div>
+    );
+}
+
+export default Friends;
